e2e26295dc35a09907635df94ed6bf23bde9688c
Ogurchick61 <greenred61@tutamail.com>
2024-11-19 17:34:55 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,38 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+        self.current_floor = 1
+
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            self.current_floor = new_floor
+            print(f'{self.name}: перемещение на {new_floor} этаж ')
+
+    def __str__(self):
+        return f'{self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __del__(self):
+        print(f'Дом в {self.name} снесён, о он останется в истории')
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+del h2
+del h3
+print(House.houses_history)
